0f43bccfb90851c000ce0e325104a3d9f033e292
supergravityy <dbtjdtn325@gmail.com>
2024-06-30 02:21:57 +0900

    02study

---
+++ 24-Cstudy02/var01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 3;
+
+	printf("%d\n",a);
+}
---
+++ 24-Cstudy02/var02.c
@@ -0,0 +1,11 @@
+void main()
+{
+	int a,b,c,d,e,f;
+	a = 1;
+	b = 2;
+	c = 3;
+	d = 4;
+	e = 5;
+	f = 6;
+	int result = a+b+c+d+e+f;
+}
